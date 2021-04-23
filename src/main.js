17cff881bdc4ff7b09f9798c9b5ce159b4e561ea
Carolina Lagerberg <carolina.lagerberg@live.se>
2021-04-23 17:09:11 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,28 @@
+window.addEventListener("load", function () {
+    
+TweenLite.set('.introline, .bigline',{x:'-103%'})
+
+new TimelineMax({repeat:-1, yoyo:true, repeatDelay:2})
+.to('.bigline',1,{x:'0%'})
+.to('.introline',1,{x:'0%'}, "+=0.3")
+
+TweenMax.to("#arrowRight", 1, {
+    x: 20,
+    repeat: 10,
+    yoyo: true,
+    ease: Sine.easeInOut,
+    repeat: -1,
+  });
+
+});
+
+
+
+
+
+
+
+ 
+
+
+
