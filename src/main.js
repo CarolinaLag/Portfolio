e4133c3f3275d634c4ac0b6dfc22e630e95bfae2
Carolina Lagerberg <carolina.lagerberg@live.se>
2021-04-24 13:45:06 +0200

    smallfix

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 window.addEventListener("load", function () {
-  
-    
-TweenLite.set('.introline, .bigline',{x:'-103%'})
+  TweenLite.set(".introline, .bigline", { x: "-103%" });
 
-new TimelineMax({repeat:-1, yoyo:true, repeatDelay:2})
-.to('.bigline',1,{x:'0%'})
-.to('.introline',1,{x:'0%'}, "+=0.3")
+  new TimelineMax({ repeat: -1, yoyo: true, repeatDelay: 2 })
+    .to(".bigline", 1, { x: "0%" })
+    .to(".introline", 1, { x: "0%" }, "+=0.3");
 
-TweenMax.to("#arrowRight", 1, {
+  TweenMax.to("#arrowRight", 1, {
     x: 20,
     repeat: 10,
     yoyo: true,
@@ -15,35 +13,34 @@ TweenMax.to("#arrowRight", 1, {
     repeat: -1,
   });
 
-  let elements = document.getElementsByClassName('txt-rotate');
-  for (let i=0; i<elements.length; i++) {
-    let toRotate = elements[i].getAttribute('data-rotate');
-    let period = elements[i].getAttribute('data-period');
+  let elements = document.getElementsByClassName("txt-rotate");
+  for (let i = 0; i < elements.length; i++) {
+    let toRotate = elements[i].getAttribute("data-rotate");
+    let period = elements[i].getAttribute("data-period");
     if (toRotate) {
       new TxtRotate(elements[i], JSON.parse(toRotate), period);
     }
   }
-  
+
   let css = document.createElement("style");
   css.type = "text/css";
   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
   document.body.appendChild(css);
-
 });
 
 document.getElementById("year").innerHTML = new Date().getFullYear();
 
-let TxtRotate = function(el, toRotate, period) {
+let TxtRotate = function (el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
   this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
+  this.txt = "";
   this.tick();
   this.isDeleting = false;
 };
 
-TxtRotate.prototype.tick = function() {
+TxtRotate.prototype.tick = function () {
   let i = this.loopNum % this.toRotate.length;
   let fullTxt = this.toRotate[i];
 
@@ -53,24 +50,25 @@ TxtRotate.prototype.tick = function() {
     this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
 
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
 
   let that = this;
   let delta = 300 - Math.random() * 100;
 
-  if (this.isDeleting) { delta /= 2; }
+  if (this.isDeleting) {
+    delta /= 2;
+  }
 
   if (!this.isDeleting && this.txt === fullTxt) {
     delta = this.period;
     this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
+  } else if (this.isDeleting && this.txt === "") {
     this.isDeleting = false;
     this.loopNum++;
     delta = 500;
   }
 
-  setTimeout(function() {
+  setTimeout(function () {
     that.tick();
   }, delta);
 };
-
