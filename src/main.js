f2d6453559b54ccb884757fe3fad5b21a3d15801
Carolina Lagerberg <carolina.lagerberg@live.se>
2021-05-03 11:05:31 +0200

    added current date and time

--- src/main.js
+++ src/main.js
@@ -72,3 +72,35 @@ TxtRotate.prototype.tick = function () {
     that.tick();
   }, delta);
 };
+
+function getDateTime() {
+  let now     = new Date(); 
+  let year    = now.getFullYear();
+  let month   = now.getMonth()+1; 
+  let day     = now.getDate();
+  let hour    = now.getHours();
+  let minute  = now.getMinutes();
+  let second  = now.getSeconds(); 
+  if(month.toString().length == 1) {
+       month = '0'+month;
+  }
+  if(day.toString().length == 1) {
+       day = '0'+day;
+  }   
+  if(hour.toString().length == 1) {
+       hour = '0'+hour;
+  }
+  if(minute.toString().length == 1) {
+       minute = '0'+minute;
+  }
+  if(second.toString().length == 1) {
+       second = '0'+second;
+  }   
+  let dateTime = year+'/'+month+'/'+day+' '+hour+':'+minute+':'+second;   
+   return dateTime;
+}
+
+setInterval( () => {
+  currentTime = getDateTime();
+  document.getElementById("dateAndTime").innerHTML = currentTime;
+}, 1000);
